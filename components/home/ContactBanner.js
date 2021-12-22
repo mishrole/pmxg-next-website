9c2c3dd54dbd3d80fab6be6c7757fafaa038592a
Mitchell Rodríguez <mitrodle@gmail.com>
2021-12-22 15:31:48 -0500

    add: Contact Banner Cta Style

--- components/home/ContactBanner.js
+++ components/home/ContactBanner.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const StyledBanner = styled.div`
-    background-color: #273e74;
-    color: #FFFFFF;
+    background-color: var(--bs-primary);
+    color: var(--bs-light);
 `;
 
 const StyledCtaButton = styled.a`
@@ -37,7 +37,6 @@ const StyledCtaButton = styled.a`
 `;
 
 export const ContactBanner = ({ translate }) => {
-
     const contactTitle = translate('home:contact-title');
     const contactSubtitle = translate('home:contact-subtitle');
     const contactCta = translate('home:contact-cta');
@@ -47,12 +46,12 @@ export const ContactBanner = ({ translate }) => {
             <StyledBanner className="py-4">
                 <Container>
                     <Row className='p-2 align-items-center justify-content-between'>
-                        <Col xs={12} md={6} className='p-3'>
+                        <Col xs={12} md={6} className='p-3 text-md-start text-center'>
                             <p className='text-uppercase fw-bold mb-0'>{contactTitle}</p>
                             <p className='mb-0'>{contactSubtitle}</p>
                         </Col>
                         <Col xs={12} md={6} className='p-3'>
-                            <div className="d-flex justify-content-end">
+                            <div className="d-flex justify-content-center justify-content-md-end">
                                 <StyledCtaButton className='btn btn-primary d-flex justify-content-center align-items-center m-0' href='https://app.pmxg.com/contact'>
                                     {contactCta}
                                 </StyledCtaButton>
