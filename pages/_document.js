19178cce422cec5258d83aa630cfce5acc73c32e
Mitchell Rodríguez <mitrodle@gmail.com>
2021-12-22 23:58:48 -0500

    fix: Abous-us description + icons + style

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;500&display=swap" rel="stylesheet"></link>
-          <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet' />
+          {/* <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet' /> */}
           {/* <link rel='icon' href='/assets/images/pmxg-large-logo.png' type='image/png'></link> */}
         </Head>
         <body>
--- pages/about-us.js
+++ pages/about-us.js
@@ -1,8 +1,12 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
+import { device } from '../util/mediaQueries';
 import Head from "next/head";
-import { Image } from "react-bootstrap";
-import styles from './../styles/About-us.module.css'
+import { Container, Row, Col } from "react-bootstrap";
+import Image from 'next/image';
+import styled from 'styled-components';
+
+import styles from './../styles/About-us.module.css';
 
 export async function getStaticProps({ locale }) {
     return {
@@ -12,34 +16,70 @@ export async function getStaticProps({ locale }) {
     }
 }
 
+const StyledContainer = styled(Container)`
+
+    @media ${device.mobileS} {
+        &&& > span > span {
+            padding: 70% 0 0 !important;
+        }
+    }
+
+    @media ${device.tablet} {
+        &&& > span > span {
+            padding: 50% 0 0 !important;
+        }
+    }
+`;
+
 const AboutUs = () => {
 
     const { t } = useTranslation();
 
     return (
-        <div className={styles.container}>
+        <>
             <Head>
                 <title>{t('about-us:title')}</title>
             </Head>
-            <div className={styles.image}>
-                <h1 className={styles.title}>{t('about-us:title')}</h1>
-            </div>
 
-            <div className={styles.textContainer}>
-                <div className={styles.description}>
-                    {
-                        t('about-us:description', { returnObjects: true }).map((element, i) => (
-                            <p key={`description-${i}`}>
-                                {t(element.content)}
-                            </p>
-                        ))
-                    }
-                </div>
-
-                <div className={styles.imageContainer}>
-                    <Image src="/assets/images/about-us/scientist.jpg" layout="responsive" />
-                </div>
+            <div className={`${styles.image} mt-5 pt-5`}>
+                <h1 className={`${styles.title}`}>{t('about-us:title')}</h1>
             </div>
+            
+            <Container className="py-5">
+                <Row className="py-5">
+                    <Col xs={12} lg={6} className="d-flex align-items-center justify-content-center flex-column">
+                        {
+                            t('about-us:description', { returnObjects: true }).map((element, i) => (
+                                
+                                    <p key={`description-${i}`}>
+                                        {t(element.content)}
+                                    </p>
+                            ))
+                        }
+                    </Col>
+                    <Col xs={12} lg={6}>
+                        <StyledContainer>
+                            <Image src="/assets/images/about-us/scientist.jpg" layout="responsive" objectFit="contain" width="200" height="200" />
+                        </StyledContainer>
+                    </Col>
+                    
+                </Row>
+                {/* <div className={styles.textContainer}>
+                    <div className={styles.description}>
+                        {
+                            t('about-us:description', { returnObjects: true }).map((element, i) => (
+                                <p key={`description-${i}`}>
+                                    {t(element.content)}
+                                </p>
+                            ))
+                        }
+                    </div>
+
+                    <div className={styles.imageContainer}>
+                        <Image src="/assets/images/about-us/scientist.jpg" layout="responsive" objectFit="contain" width="200" height="200" />
+                    </div>
+                </div> */}
+            </Container>
 
             <div className={styles.ourMission}>
                 <div className={styles.titles}>
@@ -47,26 +87,25 @@ const AboutUs = () => {
                     <h3>{t('about-us:mission.subtitle')}</h3>
                 </div>
 
-                <div className={styles.iconsContainer}>
-                    {
-                        t('about-us:mission.values', { returnObjects: true }).map((element, i) => (
-                            <div className={styles.cardIcon} key={element.title}>
-                                <div className={styles.cardHeader}>
-                                    <i className={element.icon}></i>
+                <Container>
+                    <div className={styles.iconsContainer}>
+                        {
+                            t('about-us:mission.values', { returnObjects: true }).map((element, i) => (
+                                <div className={styles.cardIcon} key={element.title}>
+                                    <div className={styles.cardHeader}>
+                                        <Image src={element.path} height={50} width={50} />
+                                    </div>
+                                    <div className={styles.cardBody}>
+                                        <h5 className={styles.subtitle}>{t(element.title)}</h5>
+                                        <p className={styles.text}>{t(element.subtitle)}</p>
+                                    </div>
                                 </div>
-                                <div className={styles.cardBody}>
-                                    <h4 className={styles.subtitle}>{t(element.title)}</h4>
-                                    <p className={styles.text}>{t(element.subttitle)}</p>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
+                            ))
+                        }
+                    </div>
+                </Container>
             </div>
-        </div>
-
-
-
+        </>
     )
 }
 
