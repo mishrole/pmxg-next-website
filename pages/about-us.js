6a322a5079e16b1b35c2a13f2dfe95606eba7a6f
Mitchell Rodríguez <mitrodle@gmail.com>
2021-12-22 19:56:50 -0500

    fix: About us Translation + Icon background style

--- pages/about-us.js
+++ pages/about-us.js
@@ -19,24 +19,21 @@ const AboutUs = () => {
     return (
         <div className={styles.container}>
             <Head>
-                <title>{t('translation:about-us')}</title>
+                <title>{t('about-us:title')}</title>
             </Head>
             <div className={styles.image}>
-                <h1 className={styles.title}>{t('translation:about-us')}</h1>
+                <h1 className={styles.title}>{t('about-us:title')}</h1>
             </div>
 
             <div className={styles.textContainer}>
                 <div className={styles.description}>
-                    <p>
-                        {t('translation:description_v1')}
-                    </p>
-                    <p>
-                        {t('translation:description_v2')}
-                    </p>
-                    <p>
-                        {t('translation:description_v3')}
-                    </p>
-
+                    {
+                        t('about-us:description', { returnObjects: true }).map((element, i) => (
+                            <p>
+                                {t(element.content)}
+                            </p>
+                        ))
+                    }
                 </div>
 
                 <div className={styles.imageContainer}>
@@ -46,47 +43,24 @@ const AboutUs = () => {
 
             <div className={styles.ourMission}>
                 <div className={styles.titles}>
-                    <h2>{t('translation:our-mission')}</h2><br />
-                    <h3>{t('translation:offer-clients')}</h3>
+                    <h2>{t('about-us:mission.title')}</h2><br />
+                    <h3>{t('about-us:mission.subtitle')}</h3>
                 </div>
 
                 <div className={styles.iconsContainer}>
-                    <div className={styles.cardIcon}>
-                        <div className={styles.cardHeader}>
-                            <i class='bx bx-star' ></i>
-                        </div>
-                        <div className={styles.cardBody}>
-                            <h4 className={styles.subtitle}>{t('translation:transparency')}</h4>
-                            <p className={styles.text}>{t('translation:transparency-message')}</p>
-                        </div>
-                    </div>
-                    <div className={styles.cardIcon}>
-                        <div className={styles.cardHeader}>
-                            <i class='bx bx-lock'></i>
-                        </div>
-                        <div className={styles.cardBody}>
-                            <h4 className={styles.subtitle}>{t('translation:security')}</h4>
-                            <p className={styles.text}>{t('translation:security-message')}</p>
-                        </div>
-                    </div>
-                    <div className={styles.cardIcon}>
-                        <div className={styles.cardHeader}>
-                            <i class='bx bx-like'></i>
-                        </div>
-                        <div className={styles.cardBody}>
-                            <h4 className={styles.subtitle}>{t('translation:service')}</h4>
-                            <p className={styles.text}>{t('translation:service-message')}</p>
-                        </div>
-                    </div>
-                    <div className={styles.cardIcon}>
-                        <div className={styles.cardHeader}>
-                            <i class='bx bxs-key'></i>
-                        </div>
-                        <div className={styles.cardBody}>
-                            <h4 className={styles.subtitle}>{t('translation:cost-effectiveness')}</h4>
-                            <p className={styles.text}>{t('translation:cost-effectiveness-message')}</p>
-                        </div>
-                    </div>
+                    {
+                        t('about-us:mission.values', { returnObjects: true }).map((element, i) => (
+                            <div className={styles.cardIcon} key={element.title}>
+                                <div className={styles.cardHeader}>
+                                    <i className={element.icon}></i>
+                                </div>
+                                <div className={styles.cardBody}>
+                                    <h4 className={styles.subtitle}>{t(element.title)}</h4>
+                                    <p className={styles.text}>{t(element.subttitle)}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
